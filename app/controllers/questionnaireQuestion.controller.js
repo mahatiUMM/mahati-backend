4f1c7a8e44f3efc8e34376aa836c74506dde4408
xyzuan <xyzuannihboss@gmail.com>
2024-07-13 17:32:20 +0700

    feat(questionnaire): Add Available Answer model

--- app/controllers/questionnaireQuestion.controller.js
+++ app/controllers/questionnaireQuestion.controller.js
@@ -1,110 +1,116 @@
-import { prisma } from "../lib/dbConnect.js"
-export * as questionnaireQuestionController from "./questionnaireQuestion.controller.js"
-import { verifyToken } from "../lib/tokenHandler.js"
+import { prisma } from "../lib/dbConnect.js";
+export * as questionnaireQuestionController from "./questionnaireQuestion.controller.js";
+import { verifyToken } from "../lib/tokenHandler.js";
 
 // Create questionnaire question
 export const createQuestionnaireQuestion = async (req, res, next) => {
   try {
-    const { questionnaire_id, question } = req.body
+    const { questionnaire_id, question, available_answer } = req.body;
 
-    const newQuestionnaireQuestion = await prisma.questionnaire_questions.create({
-      data: {
-        questionnaire_id,
-        question,
-      },
-    })
+    const newQuestionnaireQuestion =
+      await prisma.questionnaire_questions.create({
+        data: {
+          questionnaire_id,
+          available_answer,
+          question,
+        },
+      });
 
-    res.status(201).json({ success: true, data: newQuestionnaireQuestion })
+    res.status(201).json({ success: true, data: newQuestionnaireQuestion });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 // Get all questionnaire questions
 export const getAllQuestionnaireQuestions = async (req, res, next) => {
   try {
-    const data = verifyToken(req.headers.access_token)
-    if (data?.status) return res.status(data.status).json(data)
+    const data = verifyToken(req.headers.access_token);
+    if (data?.status) return res.status(data.status).json(data);
 
-    const questionnaireQuestions = await prisma.questionnaire_questions.findMany({
-      include: {
-        questionnaire: true,
-        questionnaire_answers: true,
-      },
-    })
+    const questionnaireQuestions =
+      await prisma.questionnaire_questions.findMany({
+        include: {
+          questionnaire: true,
+          questionnaire_answers: true,
+        },
+      });
 
-    res.json({ success: true, data: questionnaireQuestions })
+    res.json({ success: true, data: questionnaireQuestions });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 // Get questionnaire question by ID
 export const getQuestionnaireQuestionById = async (req, res, next) => {
   try {
-    const data = verifyToken(req.headers.access_token)
-    if (data?.status) return res.status(data.status).json(data)
+    const data = verifyToken(req.headers.access_token);
+    if (data?.status) return res.status(data.status).json(data);
 
-    const questionId = parseInt(req.params.id)
+    const questionId = parseInt(req.params.id);
 
-    const questionnaireQuestion = await prisma.questionnaire_questions.findUnique({
-      where: { id: questionId },
-      include: {
-        questionnaire: true,
-        questionnaire_answers: true,
-      },
-    })
+    const questionnaireQuestion =
+      await prisma.questionnaire_questions.findUnique({
+        where: { id: questionId },
+        include: {
+          questionnaire: true,
+          questionnaire_answers: true,
+        },
+      });
 
     if (!questionnaireQuestion) {
       return res.status(404).json({
         status: 404,
         message: "Questionnaire question not found.",
-      })
+      });
     }
 
-    res.json({ success: true, data: questionnaireQuestion })
+    res.json({ success: true, data: questionnaireQuestion });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 // Update questionnaire question by ID
 export const updateQuestionnaireQuestion = async (req, res, next) => {
   try {
-    const data = verifyToken(req.headers.access_token)
-    if (data?.status) return res.status(data.status).json(data)
+    const data = verifyToken(req.headers.access_token);
+    if (data?.status) return res.status(data.status).json(data);
 
-    const questionId = parseInt(req.params.id)
-    const { questionnaire_id, question } = req.body
+    const questionId = parseInt(req.params.id);
+    const { questionnaire_id, question } = req.body;
 
-    const updatedQuestionnaireQuestion = await prisma.questionnaire_questions.update({
-      where: { id: questionId },
-      data: {
-        questionnaire_id,
-        question,
-      },
-    })
+    const updatedQuestionnaireQuestion =
+      await prisma.questionnaire_questions.update({
+        where: { id: questionId },
+        data: {
+          questionnaire_id,
+          question,
+        },
+      });
 
-    res.json({ success: true, data: updatedQuestionnaireQuestion })
+    res.json({ success: true, data: updatedQuestionnaireQuestion });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 // Delete questionnaire question by ID
 export const deleteQuestionnaireQuestion = async (req, res, next) => {
   try {
-    const data = verifyToken(req.headers.access_token)
-    if (data?.status) return res.status(data.status).json(data)
+    const data = verifyToken(req.headers.access_token);
+    if (data?.status) return res.status(data.status).json(data);
 
-    const questionId = parseInt(req.params.id)
+    const questionId = parseInt(req.params.id);
 
-    const deletedQuestionnaireQuestion = await prisma.questionnaire_questions.delete({
-      where: { id: questionId },
-    })
+    const deletedQuestionnaireQuestion =
+      await prisma.questionnaire_questions.delete({
+        where: { id: questionId },
+      });
 
-    res.json({ success: true, data: deletedQuestionnaireQuestion })
+    res.json({ success: true, data: deletedQuestionnaireQuestion });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
