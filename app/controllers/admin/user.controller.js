5374485b184788427ba43af7aa5c9ce4914ff4bd
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-08-17 22:14:46 +0700

    feat(admin): implement profile

--- app/controllers/admin/user.controller.js
+++ app/controllers/admin/user.controller.js
@@ -23,4 +23,82 @@ export const getAllUsers = async (req, res, next) => {
   }
 }
 
+export const getUser = async (req, res, next) => {
+  try {
+    const data = verifyToken(req.headers.access_token);
+    if (data?.status) return res.status(data.status).json(data);
+
+    const user = await getUserById(data.id);
+
+    if (user.isAdmin) {
+      const userDetail = await prisma.users.findUnique({
+        where: {
+          id: data.id,
+        },
+        include: {
+          isAdmin: true,
+          questionnaire_answers: true,
+          bookmarks: true,
+          RefreshToken: true,
+        }
+      })
+
+      res.json({ success: true, data: userDetail });
+    } else {
+      res.status(403).json({
+        status: 403,
+        message: "You are not authorized to perform this action.",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const data = verifyToken(req.headers.access_token);
+    if (data?.status) return res.status(data.status).json(data);
+
+    const { username, email, number } = req.body;
+
+    let photo = req.file ? req.file.path : req.body.photo;
+
+    if (!photo) {
+      if (!username || !email || !number) {
+        return res.status(400).json({
+          status: 400,
+          message:
+            "Please provide all required fields (username, email, number).",
+        });
+      }
+    }
+
+    const user = await getUserById(data.id);
+
+    if (user.isAdmin) {
+      const updatedUser = await prisma.users.update({
+        where: {
+          id: data.id,
+        },
+        data: {
+          username,
+          email,
+          number,
+          photo,
+        },
+      });
+
+      res.json({ success: true, data: updatedUser });
+    } else {
+      res.status(403).json({
+        status: 403,
+        message: "You are not authorized to perform this action.",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
 export * as adminUserController from "./user.controller.js";
--- app/routes/admin/user.route.js
+++ app/routes/admin/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { tokenValidation } from "../../lib/tokenHandler.js";
 import { adminUserController } from "../../controllers/admin/user.controller.js";
+import { imageUploader } from "../../lib/multerStorage.js";
 
 const routes = Router({ strict: true });
 
@@ -8,6 +9,17 @@ routes.get(
   "/users",
   tokenValidation(),
   adminUserController.getAllUsers
+);
+routes.get(
+  "/profile",
+  tokenValidation(),
+  adminUserController.getUser
+);
+routes.put(
+  "/profile",
+  tokenValidation(),
+  imageUploader.single("image"),
+  adminUserController.updateUser
 );
 
 export default routes;
