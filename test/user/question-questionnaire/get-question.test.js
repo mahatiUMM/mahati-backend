991919a268575a8f938f44c814061be16456f63f
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-12-17 21:48:04 +0700

    test(question): coverage on question questionnaire

---
+++ test/user/question-questionnaire/get-question.test.js
@@ -0,0 +1,13 @@
+import supertest from "supertest";
+import app from "../../../app";
+import { prisma } from "../../../app/lib/dbConnect";
+
+describe("test GET /api/questionnaire_question", () => {
+  it("should return success true when getting all questionnaire questions", async () => {
+    const response = await supertest(app)
+      .get("/api/questionnaire_question")
+      .set("Authorization", `Bearer ${process.env.TEST_TOKEN}`);
+
+    console.log(response.body);
+  });
+});
